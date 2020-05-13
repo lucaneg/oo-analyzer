abd13589085d4f1e3a3ba001628977700e32f1d3
Luca Negrini <luca.negrini@juliasoft.com>
2020-05-13 15:11:43 +0200

    Updated automaton-strings dependency + extended replace benchmark

--- oo-analyzer/oo-analyzer-core/oo-inputs/string-benchmarks/preparedStatement.java
+++ oo-analyzer/oo-analyzer-core/oo-inputs/string-benchmarks/preparedStatement.java
@@ -1,8 +1,15 @@
 class PreparedStatement {
+	String prepare() {
+		String stm = "UPDATE user_table SET name=%1 WHERE id=%2";
+		stm = stm.replace("%1", "john");
+		stm = stm.replace("%2", "345");
+		return stm;
+	}
+	
 	String prepare(String first, String second) {
 		String stm = "UPDATE user_table SET name=%1 WHERE id=%2";
-		stm = stm.replace("%1", "the name");
-		stm = stm.replace("%2", "345");
+		stm = stm.replace("%1", first);
+		stm = stm.replace("%2", second);
 		return stm;
 	}
 }
