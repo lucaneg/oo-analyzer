4508ab9336ecd3d3f456ff9b2e14580005562f50
Luca Negrini <luca.negrini@juliasoft.com>
2020-05-17 14:05:39 +0200

    Removed hierarchical expression evaluation from sdk

--- oo-analyzer/oo-analyzer-sdk/src/main/java/it/lucaneg/oo/sdk/analyzer/analyses/impl/AbstractExpressionEvaluator.java
+++ oo-analyzer/oo-analyzer-sdk/src/main/java/it/lucaneg/oo/sdk/analyzer/analyses/impl/AbstractExpressionEvaluator.java
@@ -32,102 +32,97 @@ import it.lucaneg.oo.sdk.analyzer.analyses.ExpressionEvaluator;
 public abstract class AbstractExpressionEvaluator<L extends AbstractLattice<L>> implements ExpressionEvaluator<L> {
 
 	@Override
-	public L eval(Expression e, Environment<?, ?> env) {
-		return eval(e, env, new AbstractExpressionEvaluator<L>() {});
-	}
-	
-	@Override
-	public final L eval(Expression e, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	public final L eval(Expression e, Environment<?, ?> env) {
 		// type expression is not needed since it is used only during parsing
 		if (e instanceof ArrayAccess)
-			return evalArrayAccess((ArrayAccess) e, env, parent);
+			return evalArrayAccess((ArrayAccess) e, env);
 		else if (e instanceof Call)
-			return evalCall((Call) e, env, parent);
+			return evalCall((Call) e, env);
 		else if (e instanceof Cast)
-			return evalCast((Cast) e, env, parent);
+			return evalCast((Cast) e, env);
 		else if (e instanceof FieldAccess)
-			return evalFieldAccess((FieldAccess) e, env, parent);
+			return evalFieldAccess((FieldAccess) e, env);
 		else if (e instanceof Minus)
-			return evalMinus((Minus) e, env, parent);
+			return evalMinus((Minus) e, env);
 		else if (e instanceof NewArray)
-			return evalNewArray((NewArray) e, env, parent);
+			return evalNewArray((NewArray) e, env);
 		else if (e instanceof NewObject)
-			return evalNewObject((NewObject) e, env, parent);
+			return evalNewObject((NewObject) e, env);
 		else if (e instanceof Not)
-			return evalNot((Not) e, env, parent);
+			return evalNot((Not) e, env);
 		else if (e instanceof TypeCheck)
-			return evalTypeCheck((TypeCheck) e, env, parent);
+			return evalTypeCheck((TypeCheck) e, env);
 		else if (e instanceof Variable)
-			return evalVariable((Variable) e, env, parent);
+			return evalVariable((Variable) e, env);
 		else if (e instanceof Literal)
-			return evalLiteral((Literal) e, env, parent);
+			return evalLiteral((Literal) e, env);
 		else if (e instanceof Addition)
-			return evalAddition((Addition) e, env, parent);
+			return evalAddition((Addition) e, env);
 		else if (e instanceof Division)
-			return evalDivision((Division) e, env, parent);
+			return evalDivision((Division) e, env);
 		else if (e instanceof Module)
-			return evalModule((Module) e, env, parent);
+			return evalModule((Module) e, env);
 		else if (e instanceof Multiplication)
-			return evalMultiplication((Multiplication) e, env, parent);
+			return evalMultiplication((Multiplication) e, env);
 		else if (e instanceof Subtraction)
-			return evalSubtraction((Subtraction) e, env, parent);
+			return evalSubtraction((Subtraction) e, env);
 		else if (e instanceof And)
-			return evalAnd((And) e, env, parent);
+			return evalAnd((And) e, env);
 		else if (e instanceof Or)
-			return evalOr((Or) e, env, parent);
+			return evalOr((Or) e, env);
 		else if (e instanceof Equal)
-			return evalEqual((Equal) e, env, parent);
+			return evalEqual((Equal) e, env);
 		else if (e instanceof NotEqual)
-			return evalNotEqual((NotEqual) e, env, parent);
+			return evalNotEqual((NotEqual) e, env);
 		else if (e instanceof Greater)
-			return evalGreater((Greater) e, env, parent);
+			return evalGreater((Greater) e, env);
 		else if (e instanceof GreaterOrEqual)
-			return evalGreaterOrEqual((GreaterOrEqual) e, env, parent);
+			return evalGreaterOrEqual((GreaterOrEqual) e, env);
 		else if (e instanceof Less)
-			return evalLess((Less) e, env, parent);
+			return evalLess((Less) e, env);
 		else if (e instanceof LessOrEqual)
-			return evalLessOrEqual((LessOrEqual) e, env, parent);
+			return evalLessOrEqual((LessOrEqual) e, env);
 		else
 			return (L) env.defaultLatticeForType(e.getStaticType());
 	}
 
-	protected L evalArrayAccess(ArrayAccess array, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalArrayAccess(ArrayAccess array, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(array.getStaticType());
 	}
 
-	protected L evalCall(Call call, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalCall(Call call, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(call.getStaticType());
 	}
 
-	protected L evalCast(Cast as, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalCast(Cast as, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(as.getStaticType());
 	}
 
-	protected L evalFieldAccess(FieldAccess field, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalFieldAccess(FieldAccess field, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(field.getStaticType());
 	}
 
-	protected L evalMinus(Minus minus, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalMinus(Minus minus, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(minus.getStaticType());
 	}
 
-	protected L evalNewArray(NewArray newarr, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalNewArray(NewArray newarr, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(newarr.getStaticType());
 	}
 
-	protected L evalNewObject(NewObject newobj, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalNewObject(NewObject newobj, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(newobj.getStaticType());
 	}
 
-	protected L evalNot(Not not, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalNot(Not not, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(not.getStaticType());
 	}
 
-	protected L evalTypeCheck(TypeCheck is, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalTypeCheck(TypeCheck is, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(is.getStaticType());
 	}
 
-	protected final L evalVariable(Variable var, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected final L evalVariable(Variable var, Environment<?, ?> env) {
 		String name = ((Variable) var).getName();
 		if (env.hasApproximationFor(name))
 			return (L) env.at(name);
@@ -135,59 +130,59 @@ public abstract class AbstractExpressionEvaluator<L extends AbstractLattice<L>> 
 		return (L) env.defaultLatticeForType(var.getStaticType());
 	}
 
-	protected L evalLiteral(Literal literal, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalLiteral(Literal literal, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(literal.getStaticType());
 	}
 
-	protected L evalAddition(Addition add, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalAddition(Addition add, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(add.getStaticType());
 	}
 
-	protected L evalDivision(Division div, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalDivision(Division div, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(div.getStaticType());
 	}
 
-	protected L evalModule(Module mod, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalModule(Module mod, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(mod.getStaticType());
 	}
 
-	protected L evalMultiplication(Multiplication mul, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalMultiplication(Multiplication mul, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(mul.getStaticType());
 	}
 
-	protected L evalSubtraction(Subtraction sub, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalSubtraction(Subtraction sub, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(sub.getStaticType());
 	}
 
-	protected L evalAnd(And and, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalAnd(And and, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(and.getStaticType());
 	}
 
-	protected L evalOr(Or or, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalOr(Or or, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(or.getStaticType());
 	}
 
-	protected L evalEqual(Equal eq, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalEqual(Equal eq, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(eq.getStaticType());
 	}
 
-	protected L evalNotEqual(NotEqual ne, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalNotEqual(NotEqual ne, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(ne.getStaticType());
 	}
 
-	protected L evalGreater(Greater gt, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalGreater(Greater gt, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(gt.getStaticType());
 	}
 
-	protected L evalGreaterOrEqual(GreaterOrEqual ge, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalGreaterOrEqual(GreaterOrEqual ge, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(ge.getStaticType());
 	}
 
-	protected L evalLess(Less lt, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalLess(Less lt, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(lt.getStaticType());
 	}
 
-	protected L evalLessOrEqual(LessOrEqual le, Environment<?, ?> env, ExpressionEvaluator<?> parent) {
+	protected L evalLessOrEqual(LessOrEqual le, Environment<?, ?> env) {
 		return (L) env.defaultLatticeForType(le.getStaticType());
 	}
 }
